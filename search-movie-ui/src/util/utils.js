de713d609161d342c0d42699c1fee88690a1065e
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2024-09-28 05:04:07 -0700

    create Utilities file

---
+++ search-movie-ui/src/util/utils.js
@@ -0,0 +1,23 @@
+/* Utility functions */
+export const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export const stringToArray = (str) => {
+  const arrayOfItems = str.split(", ");
+  return arrayOfItems;
+};
+
+export const formatNumberOfVotes = (numberString) => {
+  const numStringWithoutCommas = numberString.replaceAll(",", "");
+
+  return numStringWithoutCommas >= 1000 && numStringWithoutCommas < 1000000
+    ? `${Math.floor(numStringWithoutCommas / 1000)}K`
+    : numStringWithoutCommas >= 1000000
+    ? `${Math.floor(numStringWithoutCommas / 1000000)}M`
+    : numberString;
+};
+
+export const hasData = (value) => {
+  return value !== "N/A" ? true : false;
+};
