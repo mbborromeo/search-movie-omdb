230c91234d955b96abd065864b8ece3b347e229a
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2024-10-06 00:02:45 -0700

    install Eslint and remove unneeded files

--- search-movie-ui/eslint.config.mjs
+++ search-movie-ui/eslint.config.mjs
@@ -1,11 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  // {
+  //   extends: [
+  //     'plugin:react/recommended',
+  //     'plugin:prettier/recommended'
+  //   ]
+  // },
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off'
+    }
+  }
 ];
